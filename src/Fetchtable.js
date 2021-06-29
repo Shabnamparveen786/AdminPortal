8765bd8b58e1057a4f1ea8df3fbb2ecebf7fc4ee
Shabnamparveen786 <81210108+Shabnamparveen786@users.noreply.github.com>
2021-06-29 17:16:21 +0530

    Update Fetchtable.js

--- src/Fetchtable.js
+++ src/Fetchtable.js
@@ -1,104 +1,102 @@
+import React, { useState } from 'react'
 
 
-import React, {useState } from 'react'
 
- 
- 
-const Fetchtable = () =>{
+const Fetchtable = () => {
     const [txt, setTxt] = useState("");
     const [users, setUsers] = useState([]);
-   
 
-    const inputSearch=(event) =>{
-      setTxt( event.target.value);
+
+    const inputSearch = (event) => {
+        setTxt(event.target.value);
 
     }
-    
-    const SearchBtn = async () => {
+
+    const SearchBtn = async (e) => {
+        e.preventDefault();
         setTxt(txt)
-        console.log(txt);
-        const url = "http://65.0.216.210:5000/search/people?query={query}".replace("{query}",txt)
+       
+
+        console.log(txt)
+        var url = ""
+        if (txt === "") {
+            url = "http://65.0.216.210:5000/search/people"
+        }
+        else {
+            url = "http://65.0.216.210:5000/search/people?query={query}".replace("{query}", txt)
+        }
+
         console.log(url)
-        
-    
-        const response = await  fetch(url,
-               {method :'GET',
-                headers:{
+
+
+        const response = await fetch(url,
+            {
+                method: 'GET',
+                headers: {
                     'Accept': 'Application/json',
-                    'content-type' : 'Application/json',
+                    'content-type': 'Application/json',
                     'userId': 'web_user',
                     'auth': 'we-are-metby'
                 }
-            }) 
-      
-        // const users = await response.json();
-        // console.log(users);
+            })
 
-        // setUsers(users);
-        if(response.ok){
+    
+        if (response.ok) {
             const user = await response.json();
             console.log(user);
+             console.log("hello");
+            console.log(user.records);
+            setUsers(user.records);
 
-            console.log(user.records[0].name);
-            console.log(user.records.length);
-           
-             setUsers({users}); 
-              
+
         }
-        
-        
-        
-       console.log({users});
-
-      /* console.log(users.records.length);
-       return  users.records.length > 0 ?(   
-                <>
-                 <table>
-                           <thead>
-                              <tr>
-                                    <th>ProfilePic</th>
-                                    <th>UserId</th>
-                                    <th>Name</th>
-                                    <th>Phone Number</th>
-                              </tr>
-
-                           </thead>
-                           <tbody>
-                                  
-                           </tbody>
-                       </table>
-                </>
-             ):
-             <div>No users</div>
-             */
-        
 
 }
+   
+    console.log(users);
 
-//   users.records.map(val =>{
-//             return (
-//                 <td>{val.name}</td>
-//             )
-//         })
-       
-    
-
-    
-
+     
     return (
+        
         <>
             <div>
                 <input type="text" onChange={inputSearch} placeholder="Enter name here..." />
                 <button onClick={SearchBtn}>Search</button>
             </div>
-            <div>
-                {/* <p>  {users.records}</p> */}
-            </div>
 
-            </>
-           
-            )
-    
+            <div>
+                <table>
+                    <thead> 
+                         <tr>
+                               <th>UserId</th>
+                               <th>Name</th>
+                               <th>phone Number</th>
+                               <th>profilePic</th>
+                         </tr>
+                    </thead>
+                    <tbody>
+                            {
+                                
+                          users.map((val)=>
+                            <tr key={val.userId}>
+                               <td>{val.userId}</td>
+                               <td>{val.name}</td>
+                               <td>{val.phoneNumber}</td>
+                               <td>
+                                  <img src={val.profilePic} alt="error"/>
+                               </td>
+                            </tr>
+                          )
+                        }
+                    </tbody>
+                </table>
+            </div>
+          
+
+        </>
+
+    )
+
 }
 
 export default Fetchtable;
